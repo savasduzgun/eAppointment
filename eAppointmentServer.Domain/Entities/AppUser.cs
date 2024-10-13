93ea39c1218fed4af51065de3d49248dd177de6c
savasduzgun <savasdzgn@gmail.com>
2024-10-13 21:05:00 +0300

    AppUser yazıldı.

--- eAppointmentServer.Domain/Entities/AppUser.cs
+++ eAppointmentServer.Domain/Entities/AppUser.cs
@@ -2,7 +2,12 @@
 
 namespace eAppointmentServer.Domain.Entities
 {
-    public sealed class AppUser : IdentityUser
+    public sealed class AppUser : IdentityUser<Guid>
     {
+        //identity paketinde olmayan propertyler
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        //FullName db ye kaydolmaz çağrınca FirstName+LastName olarak gelir
+        public string FullName => string.Join(" ", FirstName, LastName);
     }
 }
