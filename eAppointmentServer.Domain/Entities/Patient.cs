9c1fb445c27b65ff9dc6a45c5ee7706b16b4ed50
savasduzgun <savasdzgn@gmail.com>
2024-10-14 07:26:41 +0300

    Enums ve Doctor entity yazıldı.

--- eAppointmentServer.Domain/Entities/Patient.cs
+++ eAppointmentServer.Domain/Entities/Patient.cs
@@ -10,6 +10,7 @@
         public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string FullName => string.Join(" ", FirstName, LastName);
         public string IdentityNumber { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty; 
         public string Town { get; set; } = string.Empty; 
---
+++ eAppointmentServer.Domain/Enums/DepertmentEnum.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.SmartEnum;
+
+namespace eAppointmentServer.Domain.Enums
+{
+    public sealed class DepertmentEnum : SmartEnum<DepertmentEnum>
+    {
+        public static readonly DepertmentEnum Acil = new("Acil", 1);
+        public static readonly DepertmentEnum Radyoloji = new("Radyoloji", 2);
+        public static readonly DepertmentEnum Kardioloji = new("Kardioloji", 3);
+        public static readonly DepertmentEnum Dermatoloji = new("Dermatoloji", 4);
+        public static readonly DepertmentEnum Endokrinoloji = new("Endokrinoloji", 5);
+        public static readonly DepertmentEnum Gastroenteroloji = new("Gastroenteroloji", 6);
+        public static readonly DepertmentEnum GenelCerrahi = new("Genel Cerrahi", 7);
+        public static readonly DepertmentEnum JinekolojiveObstetrik = new("Jinekoloji ve Obstetrik", 8);
+        public static readonly DepertmentEnum Hematoloji = new("Hematoloji", 9);
+        public static readonly DepertmentEnum EnfeksiyonHastalıklari = new("Enfeksiyon Hastalıkları", 10);
+        public static readonly DepertmentEnum Nefroloji = new("Nefroloji", 11);
+        public static readonly DepertmentEnum Nöroloji = new("Nöroloji", 12);
+        public static readonly DepertmentEnum Ortopedi = new("Ortopedi", 13);
+        public static readonly DepertmentEnum Pediatri = new("Pediatri", 14);
+        public static readonly DepertmentEnum Psikiyatri = new("Psikiyatri", 15);
+        public static readonly DepertmentEnum Pulmonoloji = new("Pulmonoloji", 16);
+        public DepertmentEnum(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
