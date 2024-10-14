2d88bbc3aa845559477fedf00e19916610efbd0a
savasduzgun <savasdzgn@gmail.com>
2024-10-14 07:06:34 +0300

    Patient entity si yazıldı.

---
+++ eAppointmentServer.Domain/Entities/Patient.cs
@@ -0,0 +1,18 @@
+﻿namespace eAppointmentServer.Domain.Entities
+{
+    public sealed class Patient
+    {
+        //patient nesnesi her üretildiğinde otomatik guid oluşur
+        public Patient() 
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string IdentityNumber { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty; 
+        public string Town { get; set; } = string.Empty; 
+        public string FullAdress { get; set; } = string.Empty;
+    }
+}
