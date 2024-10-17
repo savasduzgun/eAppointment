7ca7559be18bcb142a3c2545e0f19b81c636e870
savasduzgun <savasdzgn@gmail.com>
2024-10-17 23:46:39 +0300

    Appointment class ı yazıldı.

--- eAppointmentServer.Domain/Entities/Appointment.cs
+++ eAppointmentServer.Domain/Entities/Appointment.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eAppointmentServer.Domain.Entities
+﻿namespace eAppointmentServer.Domain.Entities
 {
-    internal class Appointment
+    public sealed class Appointment
     {
+        public Appointment()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+
+        public Guid DoctorId { get; set; }
+        public Doctor? Doctor { get; set; }
+
+        public Guid PatientId { get; set; }
+        public Patient? Patient { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
