0b61dc74088fe9c37021f1ba537a6b4aad1598f4
savasduzgun <savasdzgn@gmail.com>
2024-10-23 21:59:21 +0300

    Kullanıcı girişi için istenecek propertyler yazıldı.

--- eAppointmentServer.Application/Features/Auth/Login/LoginCommand.cs
+++ eAppointmentServer.Application/Features/Auth/Login/LoginCommand.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace eAppointmentServer.Application.Features.Auth.Login
+﻿namespace eAppointmentServer.Application.Features.Auth.Login
 {
     //Kullanıcı giriş için gereken değerler istenecek
     public sealed record LoginCommand(
-        );
+        string UserNameOrEmail,
+        string Password);
     
     
 }
