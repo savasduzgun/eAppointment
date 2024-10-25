213dde719679060862fe40b4b0c20711afcce10d
savasduzgun <savasdzgn@gmail.com>
2024-10-25 23:43:32 +0300

    LoginCommand class ı yazıldı.

--- eAppointmentServer.Application/Features/Auth/Login/LoginCommand.cs
+++ eAppointmentServer.Application/Features/Auth/Login/LoginCommand.cs
@@ -1,9 +1,10 @@
-﻿namespace eAppointmentServer.Application.Features.Auth.Login
+﻿using MediatR;
+using TS.Result;
+
+namespace eAppointmentServer.Application.Features.Auth.Login
 {
     //Kullanıcı giriş için gereken değerler istenecek
     public sealed record LoginCommand(
         string UserNameOrEmail,
-        string Password);
-    
-    
+        string Password) : IRequest<Result<LoginCommandResponse>>;
 }
