506b1d865cc1ba77414de3affdb74ed7ac28cf9e
savasduzgun <savasdzgn@gmail.com>
2024-10-26 23:53:08 +0300

    IJwtProvider class

--- eAppointmentServer.Application/Services/IJwtProvider.cs
+++ eAppointmentServer.Application/Services/IJwtProvider.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eAppointmentServer.Domain.Entities;
 
 namespace eAppointmentServer.Application.Services
 {
-    internal interface IJwtProvider
+    public interface IJwtProvider
     {
+        string CreateToken(AppUser user);
     }
 }
