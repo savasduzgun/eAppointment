e073b89585af41adfbace77bcb08c3109fa1fa8a
savasduzgun <savasdzgn@gmail.com>
2024-10-26 23:58:29 +0300

    IJwtProvider inject edildi.

--- eAppointmentServer.Application/Features/Auth/Login/LoginCommandHandler.cs
+++ eAppointmentServer.Application/Features/Auth/Login/LoginCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using eAppointmentServer.Domain.Entities;
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -6,7 +7,7 @@ using TS.Result;
 
 namespace eAppointmentServer.Application.Features.Auth.Login
 {
-    internal sealed class LoginCommandHandler(UserManager<AppUser> userManager) : IRequestHandler<LoginCommand, Result<LoginCommandResponse>>
+    internal sealed class LoginCommandHandler(UserManager<AppUser> userManager, IJwtProvider jwtProvider) : IRequestHandler<LoginCommand, Result<LoginCommandResponse>>
     {
         public async Task<Result<LoginCommandResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
@@ -25,7 +26,10 @@ namespace eAppointmentServer.Application.Features.Auth.Login
                 return Result<LoginCommandResponse>.Failure("Password is wrong");
             }
 
-            return Result<LoginCommandResponse>.Succeed(new ("Token"));
+            string token = jwtProvider.CreateToken(appUser);
+            LoginCommandResponse response = new(token);
+
+            return Result<LoginCommandResponse>.Succeed(response);
         }
     }
 }
