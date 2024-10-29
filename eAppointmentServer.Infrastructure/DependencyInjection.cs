06338316fc18901593deae4b4dd47543dc7938ae
savasduzgun <savasdzgn@gmail.com>
2024-10-29 20:57:18 +0300

    Infrastructure Identity.UI paketi kuruldu.

--- eAppointmentServer.Infrastructure/DependencyInjection.cs
+++ eAppointmentServer.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eAppointmentServer.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace eAppointmentServer.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
+            });
             return services;
         }
     }
