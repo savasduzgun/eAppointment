e77f92dc9597aabf045fb735775ef5395fb0e240
SavasDuzgun <savasdzgn@gmail.com>
2024-10-30 15:17:53 +0300

    patients tablosunun özellikleri verildi.

---
+++ eAppointmentServer.Infrastructure/Configurations/PatientConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using eAppointmentServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAppointmentServer.Infrastructure.Configurations
+{
+    internal sealed class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        //patients tablosuna ayar vermek istenirse;
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
+            builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+            builder.Property(p => p.City).HasColumnType("varchar(50)");
+            builder.Property(p => p.Town).HasColumnType("varchar(50)");
+            builder.Property(p => p.FullAdress).HasColumnType("varchar(450)");
+            builder.Property(p => p.IdentityNumber).HasColumnType("varchar(11)");
+            builder.HasIndex(p => p.IdentityNumber).IsUnique();
+        }
+    }
+}
