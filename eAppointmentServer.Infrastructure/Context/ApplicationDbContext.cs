5a2ca77b22703d482f24be1ca04cd11367240525
SavasDuzgun <savasdzgn@gmail.com>
2024-10-30 14:55:36 +0300

    Patients tablosu

--- eAppointmentServer.Infrastructure/Context/ApplicationDbContext.cs
+++ eAppointmentServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -13,5 +13,7 @@ namespace eAppointmentServer.Infrastructure.Context
             
         }
         public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+
     }
 }
