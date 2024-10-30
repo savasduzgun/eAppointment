2a33c93e95e2386efbe0798253ee90f531604d58
SavasDuzgun <savasdzgn@gmail.com>
2024-10-30 15:13:18 +0300

    Db de Doctors table ına özellik verildi.

---
+++ eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using eAppointmentServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAppointmentServer.Infrastructure.Configurations
+{
+    internal sealed class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+    {
+        //doktorlar tablosuna ayar vermek istenirse;
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+            builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
+            builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+            //builder.HasIndex(x => x.FirstName).IsUnique();
+            //builder.HasIndex(x => x.FirstName).IsUnique();
+            //builder.HasIndex(p => new { p.FirstName, p.LastName }).IsUnique();
+        }
+    }
+}
