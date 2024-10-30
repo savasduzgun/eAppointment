5a4360d143691b6fcda359cacdd5315bbd64e612
SavasDuzgun <savasdzgn@gmail.com>
2024-10-30 15:20:20 +0300

    AppoinmentConfiguration class ı eklendi.

---
+++ eAppointmentServer.Infrastructure/Configurations/AppointmentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using eAppointmentServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eAppointmentServer.Infrastructure.Configurations
+{
+    internal sealed class AppointmentConfiguration : IEntityTypeConfiguration<Appointment>
+    {
+        //appointment tablosuna ayar vermek istenirse;
+        public void Configure(EntityTypeBuilder<Appointment> builder)
+        {
+        
+        }
+    }
+}
