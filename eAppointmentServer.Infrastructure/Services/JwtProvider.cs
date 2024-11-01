8981c76f0434ab8d7a3d6c63c416bb15aba9239a
SavasDuzgun <savasdzgn@gmail.com>
2024-11-01 16:46:36 +0300

    JwtProvider class ı IJwtProvider interface inden kalıtıldı.

--- eAppointmentServer.Infrastructure/Services/JwtProvider.cs
+++ eAppointmentServer.Infrastructure/Services/JwtProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace eAppointmentServer.Infrastructure.Services
 {
-    internal class JwtProvider
+    internal sealed class JwtProvider : IJwtProvider
     {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
