ec49259a9d13cfb822f90984209ebfa92693e4cc
savasduzgun <savasdzgn@gmail.com>
2024-11-05 18:08:21 +0300

    Cors policy yazıldı.

--- eAppointmentServer.WebAPI/Program.cs
+++ eAppointmentServer.WebAPI/Program.cs
@@ -1,7 +1,11 @@
+using DefaultCorsPolicyNugetPackage;
 using eAppointmentServer.Application;
 using eAppointmentServer.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//api nin dýþarýdan kullanýlabilmesi için cors politikasý, tek bir uygulama var o da api yi tüketeceði için herkese açýk bir endpoint gibi yazýlýr nuget paket kurulur. DefaultCorsPolicy
+builder.Services.AddDefaultCors();
 
 builder.Services.AddApplication(); //DI extension metodu
 builder.Services.AddInfrastructure(builder.Configuration); //DI extension metodu
@@ -18,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
