27efc844dd1bffa67ec6abb561346a8ca96ee14b
savasduzgun <savasdzgn@gmail.com>
2024-11-05 22:47:59 +0300

    bu class tüm controller için inherit edilen bir base class

--- eAppointmentServer.WebAPI/Abstractions/ApiController.cs
+++ eAppointmentServer.WebAPI/Abstractions/ApiController.cs
@@ -1,6 +1,18 @@
-﻿namespace eAppointmentServer.WebAPI.Abstractions
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eAppointmentServer.WebAPI.Abstractions
 {
-    public class ApiController
+    //bu class tüm controller için inherit edilen bir base class
+    [Route("api/[controller]/[action]")] //metod ismi endpoint de görünsün diye action eklenir.
+    [ApiController]
+    public abstract class ApiController : ControllerBase
     {
+        public readonly IMediator _mediator;
+
+        protected ApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
     }
 }
